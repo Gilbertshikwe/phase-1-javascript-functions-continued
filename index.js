3a5a6f3060fa8765ea5d36ddbb25ac4b1a5e4459
Gilbert <shikweg@gmail.com>
2023-12-15 21:25:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(do1="roller-skate"){
+    return `This Saturday, I want to ${do1}!`  
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+const mondayWork = function(do2="go to the office"){
+    let message = `This Monday, I will ${do2}.`
+    return message
+}
+mondayWork()
+mondayWork("work from home")
+function wrapAdjective(special,special2){
+    let part1 = "a hard worker"
+    let part2 = "||a dedicated programmer||"
+    if (special==="*"){
+    return function(){
+    return `You are ${special}${part1}${special}!`
+    }}
+    else {
+        return function (){
+        return `You are ${part2}!`
+        }
+    }
+    
+}
+wrapAdjective("*","||")()
+
+
